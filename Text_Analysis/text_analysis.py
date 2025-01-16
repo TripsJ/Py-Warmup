25ae022bf6cc1f8fe1d00625ebabd040d32b1d25
Jérôme Trips <jerome.trips@tutanota.com>
2025-01-16 02:08:43 +0100

    wrote main and text analysis functions, main is not complete

--- Text_Analysis/text_analysis.py
+++ Text_Analysis/text_analysis.py
@@ -1,4 +1,27 @@
-intext = input("Get me some text to analyze: ")
-while True:
+def check_text():
+    intext = input("Get me some text to analyze: ")
+    if intext.isascii():
+        return intext
+    else:
+        raise ValueError
 
-print("test")
+def analyse_text(text):
+  store = {}
+  for letter in text:
+    if letter in store:
+      store[letter]+=1
+    else:
+      store[letter] = 1
+  return store
+
+def main():
+  while True:
+    try:
+      text = check_text()
+      break
+    except ValueError:
+        print("Non ASCII character detected, try again")
+        continue
+  print(analyse_text(text))
+
+main()
