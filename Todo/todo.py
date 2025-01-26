3436c84c2ea859c62d4080891d3a5e44b668fd7e
Jérôme Trips <jerome.trips@tutanota.com>
2025-01-26 23:28:03 +0100

    menu implemented

--- Todo/todo.py
+++ Todo/todo.py
@@ -8,19 +8,52 @@ def add_task(tasklist: list)-> list:
   if is_not_blank(task):
     tasklist.append(task)
     return tasklist
+  else:
+    return tasklist
 
 def remove_task(tasklist: list)-> list:
   selected_task = int(input("which task do you want to remove (Index N°)?: "))
-  if tasklist[selected_task]:
+  try:
      print(f"{tasklist.pop(selected_task)} has been removed")
-  else:
+  except IndexError:
      print("The specified task does not exsist")
   return tasklist
 
+def display(tasklist:list)->None:
+  if not tasklist:
+    print ("YOUR ALL DONE")
+  else:
+    for count, task in enumerate(tasklist):
+      print (f"{count}. {task}")
+
+def menu(tasklist)->None:  
+  while True:
+    display(tasklist)
+    print("")
+    print("Please choose your action")
+    print("Press A to add a task")
+    print("Press R to remove a task")
+    print("Press S to save the current task list")
+    print ("Press Q to exit the program")
+    print("")
+    action = input("Please select your action: ").upper()
+
+    match action:
+      case "A":
+        tasklist = add_task(tasklist)
+      case "R":
+        tasklist = remove_task(tasklist)
+      case "S":
+        print("not yet implemented")
+      case "Q":
+        print("Goodbye")
+        break
+
 def main():
-    print("Hello")
-    print(add_task(tasklist))
-    print(remove_task(tasklist))
+    print ("Welcome User")
+    print("")
+    menu(tasklist)
+
 
 if __name__ == "__main__":
     main()
